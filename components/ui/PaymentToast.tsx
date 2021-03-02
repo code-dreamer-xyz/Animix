f64bf62dbb18ca14bdba22c16915da6f9b4a4d8f
Code Dreamer <codedreamer615@gmail.com>
2021-03-02 21:04:05 +0100

    add check for movie existance in user collection

---
+++ components/ui/PaymentToast.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import Link from 'next/link'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCheck } from '@fortawesome/free-solid-svg-icons'
+
+const PaymentToast = () => {
+    return (
+        <div className="p-8 text-center">
+            <div className="rounded-full mx-auto bg-primary w-16 h-16 text-white flex items-center justify-center mb-6">
+                <FontAwesomeIcon icon={faCheck} className="text-2xl" />
+            </div>
+            <p className="text-2xl font-sans font-bold mb-6">
+                Payment Successfully Confiremed
+            </p>
+            <div className="flex space-x-6">
+                <Link href="/movies">
+                    <a className="bg-primary text-white px-4 py-2 font-sans font-lg">
+                        back to Movies
+                    </a>
+                </Link>
+                <Link href="/">
+                    <a className="bg-theme text-white px-4 py-2 font-sans font-lg">
+                        go to Dashboard
+                    </a>
+                </Link>
+            </div>
+        </div>
+    )
+}
+
+export default PaymentToast
--- pages/checkout/[movieId].tsx
+++ pages/checkout/[movieId].tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import WithAuth from '../../components/WithAuth'
 
 import { useRouter } from 'next/router'
@@ -8,13 +8,24 @@ import { auth, firestore } from '../../lib/firebase'
 import { usePaymentInputs } from 'react-payment-inputs'
 
 import toast from 'react-hot-toast'
-import Link from 'next/link'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCheck } from '@fortawesome/free-solid-svg-icons'
+
+import PaymentToast from '../../components/ui/PaymentToast'
+import { useDocumentOnce } from 'react-firebase-hooks/firestore'
 
 const Checkout = () => {
     const router = useRouter()
     const { movieId } = router.query
+
+    const stringMovieId = typeof movieId === 'string' ? movieId : movieId[0]
+
+    const userMovieRef = firestore
+        .collection('users')
+        .doc(auth.currentUser.uid)
+        .collection('user_movies')
+        .doc(stringMovieId)
+
+    //check if the user already both the movie before
+    const [movieExistsInCollection] = useDocumentOnce(userMovieRef)
 
     const {
         meta,
@@ -50,7 +61,7 @@ const Checkout = () => {
                 .collection('users')
                 .doc(uid)
                 .collection('user_movies')
-                .doc()
+                .doc(stringMovieId)
 
             const userMovie = {
                 id: movie[0].id,
@@ -62,28 +73,7 @@ const Checkout = () => {
 
             movie && (await userRef.set(userMovie))
 
-            toast((t) => (
-                <div className="p-8 text-center">
-                    <div className="rounded-full mx-auto bg-primary w-16 h-16 text-white flex items-center justify-center mb-6">
-                        <FontAwesomeIcon icon={faCheck} className="text-2xl" />
-                    </div>
-                    <p className="text-2xl font-sans font-bold mb-6">
-                        Payment Successfully Confiremed
-                    </p>
-                    <div className="flex space-x-6">
-                        <Link href="/movies">
-                            <a className="bg-primary text-white px-4 py-2 font-sans font-lg">
-                                back to Movies
-                            </a>
-                        </Link>
-                        <Link href="/">
-                            <a className="bg-theme text-white px-4 py-2 font-sans font-lg">
-                                go to Dashboard
-                            </a>
-                        </Link>
-                    </div>
-                </div>
-            ))
+            toast((t) => <PaymentToast />)
         } else {
             toast.error('Please enter valid info')
         }
@@ -92,84 +82,89 @@ const Checkout = () => {
     return (
         <WithAuth>
             <section className="min-h-screen bg-theme flex items-center">
-                <div className="max-w-screen-md w-full mx-auto">
-                    <form
-                        onSubmit={handleSubmit}
-                        className="border border-primary "
-                    >
-                        <div className="bg-primary text-center">
-                            <h2 className="text-2xl font-poppins font-bold text-white py-4">
-                                Payment Details
-                            </h2>
-                        </div>
-                        <div className="p-6 flex flex-col space-y-4">
-                            <label
-                                htmlFor="Card Number"
-                                className="font-sans text-white text-xl mb-2"
-                            >
-                                Card Number
-                            </label>
-                            <input
-                                className="rounded text-white border border-gray-500 w-full p-2 bg-transparent mb-2"
-                                {...getCardNumberProps({
-                                    onChange: handleInputChange,
-                                })}
-                                value={data.cardNumber}
-                            />
-                            {meta.touchedInputs.cardNumber &&
-                                meta.erroredInputs.cardNumber && (
-                                    <span className="text-primary font-sans text-lg mb-2">
-                                        {meta.erroredInputs.cardNumber}
-                                    </span>
-                                )}
-                            <label
-                                htmlFor="Card Number"
-                                className="font-sans text-white text-xl mb-2"
-                            >
-                                Expirity Date
-                            </label>
-                            <input
-                                className="rounded text-white border border-gray-500 w-full p-2 bg-transparent mb-2"
-                                {...getExpiryDateProps({
-                                    onChange: handleInputChange,
-                                })}
-                                value={data.expiryDate}
-                            />
-                            {meta.touchedInputs.expiryDate &&
-                                meta.erroredInputs.expiryDate && (
-                                    <span className="text-primary font-sans text-lg mb-2">
-                                        {meta.erroredInputs.expiryDate}
-                                    </span>
-                                )}
-                            <label
-                                htmlFor="Cvc"
-                                className="font-sans text-white text-xl mb-2"
-                            >
-                                Cvc Number
-                            </label>
-                            <input
-                                className="rounded text-white border border-gray-500 w-full p-2 bg-transparent"
-                                {...getCVCProps({
-                                    onChange: handleInputChange,
-                                })}
-                                value={data.cvc}
-                            />
-                            {meta.touchedInputs.cvc &&
-                                meta.erroredInputs.cvc && (
-                                    <span className="text-primary font-sans text-lg">
-                                        {meta.erroredInputs.cvc}
-                                    </span>
-                                )}
-                            <button
-                                type="submit"
-                                className="disabled:cursor-not-allowed bg-primary text-white font-sans px-6 py-2 mt-6 rounded self-center"
-                                disabled={meta.isTouched && meta.error}
-                            >
-                                Submit
-                            </button>
-                        </div>
-                    </form>
-                </div>
+                {movieExistsInCollection?.exists && (
+                    <p className="text-white">Movie In</p>
+                )}
+                {!movieExistsInCollection?.exists && (
+                    <div className="max-w-screen-md w-full mx-auto">
+                        <form
+                            onSubmit={handleSubmit}
+                            className="border border-primary "
+                        >
+                            <div className="bg-primary text-center">
+                                <h2 className="text-2xl font-poppins font-bold text-white py-4">
+                                    Payment Details
+                                </h2>
+                            </div>
+                            <div className="p-6 flex flex-col space-y-4">
+                                <label
+                                    htmlFor="Card Number"
+                                    className="font-sans text-white text-xl mb-2"
+                                >
+                                    Card Number
+                                </label>
+                                <input
+                                    className="rounded text-white border border-gray-500 w-full p-2 bg-transparent mb-2"
+                                    {...getCardNumberProps({
+                                        onChange: handleInputChange,
+                                    })}
+                                    value={data.cardNumber}
+                                />
+                                {meta.touchedInputs.cardNumber &&
+                                    meta.erroredInputs.cardNumber && (
+                                        <span className="text-primary font-sans text-lg mb-2">
+                                            {meta.erroredInputs.cardNumber}
+                                        </span>
+                                    )}
+                                <label
+                                    htmlFor="Card Number"
+                                    className="font-sans text-white text-xl mb-2"
+                                >
+                                    Expirity Date
+                                </label>
+                                <input
+                                    className="rounded text-white border border-gray-500 w-full p-2 bg-transparent mb-2"
+                                    {...getExpiryDateProps({
+                                        onChange: handleInputChange,
+                                    })}
+                                    value={data.expiryDate}
+                                />
+                                {meta.touchedInputs.expiryDate &&
+                                    meta.erroredInputs.expiryDate && (
+                                        <span className="text-primary font-sans text-lg mb-2">
+                                            {meta.erroredInputs.expiryDate}
+                                        </span>
+                                    )}
+                                <label
+                                    htmlFor="Cvc"
+                                    className="font-sans text-white text-xl mb-2"
+                                >
+                                    Cvc Number
+                                </label>
+                                <input
+                                    className="rounded text-white border border-gray-500 w-full p-2 bg-transparent"
+                                    {...getCVCProps({
+                                        onChange: handleInputChange,
+                                    })}
+                                    value={data.cvc}
+                                />
+                                {meta.touchedInputs.cvc &&
+                                    meta.erroredInputs.cvc && (
+                                        <span className="text-primary font-sans text-lg">
+                                            {meta.erroredInputs.cvc}
+                                        </span>
+                                    )}
+                                <button
+                                    type="submit"
+                                    className="disabled:cursor-not-allowed bg-primary text-white font-sans px-6 py-2 mt-6 rounded self-center"
+                                    disabled={meta.isTouched && meta.error}
+                                >
+                                    Submit
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                )}
             </section>
         </WithAuth>
     )
