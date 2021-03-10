23c5f7668596a6ac81b240a719241ebc260d81f2
Code Dreamer <codedreamer615@gmail.com>
2021-03-10 11:25:44 +0100

    clean up

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -4,12 +4,7 @@ import 'firebase/firestore'
 import 'firebase/storage'
 
 const firebaseConfig = {
-    apiKey: 'AIzaSyBpoXymNPxZn-Fzs9UO2yyf-eKC5ZXaUKw',
-    authDomain: 'orcanime-a918a.firebaseapp.com',
-    projectId: 'orcanime-a918a',
-    storageBucket: 'orcanime-a918a.appspot.com',
-    messagingSenderId: '445462942565',
-    appId: '1:445462942565:web:63489cf44a68b38beb478c',
+    // add your Firebase config
 }
 
 if (!firebase.apps.length) {
