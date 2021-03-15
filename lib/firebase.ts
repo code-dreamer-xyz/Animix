1896b473058ea7adfa146f21390cd15bbf6ea84e
Code Dreamer <codedreamer615@gmail.com>
2021-03-15 11:21:44 +0100

    add firebase rules

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -5,7 +5,12 @@ import 'firebase/storage'
 import toast from 'react-hot-toast'
 
 const firebaseConfig = {
-    /// add config 
+    apiKey: "AIzaSyBpoXymNPxZn-Fzs9UO2yyf-eKC5ZXaUKw",
+    authDomain: "orcanime-a918a.firebaseapp.com",
+    projectId: "orcanime-a918a",
+    storageBucket: "orcanime-a918a.appspot.com",
+    messagingSenderId: "445462942565",
+    appId: "1:445462942565:web:63489cf44a68b38beb478c"
 }
 
 if (!firebase.apps.length) {
