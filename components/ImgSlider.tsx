f13fbb291fc98517ab012cfed48b2bda8688df91
Code Dreamer <codedreamer615@gmail.com>
2022-04-16 17:09:56 +0100

    minor changes

--- components/ImgSlider.tsx
+++ components/ImgSlider.tsx
@@ -1,40 +1,34 @@
 import Link from 'next/link'
-import { useState } from 'react'
 import Slider from 'react-slick'
 import Image from 'next/image'
 
 const ImgSlider = ({ movies }) => {
-    const [imageIndex, setImageIndex] = useState(0)
-
     const settings = {
         infinite: true,
         lazyLoad: true,
         autoplay: true,
-        speed: 300,
-        slidesToShow: 3,
+        speed: 500,
+        slidesToShow: 4,
         centerMode: true,
         centerPadding: 0,
-        dots: true,
+        dots: false,
         arrows: false,
-        beforeChange: (current: number, next: number) => setImageIndex(next),
     }
 
     return (
         <Slider {...settings}>
-            {movies?.map((movie, index) => (
+            {movies?.map((movie) => (
                 <Link href={`/${movie.id}`} key={movie.id}>
-                    <a>
+                    <a className="relative inline-block">
                         <Image
                             src={movie.img}
                             alt="anime"
-                            className={`transition-transform  duration-300 ${
-                                index === imageIndex
-                                    ? ' transform scale-95'
-                                    : 'transform scale-75'
-                            }`}
-                            width={270}
-                            height={380}
+                            width={120}
+                            height={150}
                         />
+                        <span className="absolute top-0 left-0  h-full w-full text-white text-xs font-poppins font-bold flex items-end text-center p-1 pb-3 justify-center bg-overlay">
+                            {movie.title}
+                        </span>
                     </a>
                 </Link>
             ))}
