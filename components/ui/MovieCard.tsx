8375d21751904738a3ab5fe3dae00d78fe80fd71
Code Dreamer <codedreamer615@gmail.com>
2022-04-17 16:00:10 +0100

    redesign movie card

--- components/ui/MovieCard.tsx
+++ components/ui/MovieCard.tsx
@@ -2,6 +2,8 @@ import { motion } from 'framer-motion'
 
 import { slideUpVariants, textVariants } from '../../helpers/animation'
 import Image from 'next/image'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStar } from '@fortawesome/free-solid-svg-icons'
 
 interface MovieProps {
     img: string
@@ -12,40 +14,44 @@ interface MovieProps {
 
 const MovieCard: React.FC<MovieProps> = ({ img, title, genre }) => {
     return (
-        <motion.div className="text-center">
-            <motion.div
-                whileHover={{
-                    position: 'relative',
-                    zIndex: 1,
-                    scale: 1.1,
-                    transition: {
-                        duration: 0.2,
-                    },
-                }}
-                whileTap={{ scale: 0.9 }}
-                variants={slideUpVariants}
-                className="mb-2"
-            >
-                <Image
-                    className="rounded-md w-80 h-96 block mx-auto"
-                    src={img}
-                    alt="movie"
-                    width={320}
-                    height={384}
-                />
-            </motion.div>
+        <motion.div
+            whileHover={{
+                zIndex: 1,
+                scale: 1.02,
+                transition: {
+                    duration: 0.2,
+                },
+            }}
+            whileTap={{ scale: 0.9 }}
+            variants={slideUpVariants}
+            className="mb-2 relative w-full"
+        >
+            <Image
+                className="rounded-md mx-auto"
+                src={img}
+                alt="movie"
+                width={280}
+                height={370}
+            />
+
             <motion.p
                 variants={textVariants}
-                className=" text-2xl font-sans font-bold text-primary mb-2 px-6"
+                className="font-sans text-sm font-bold mb-1 text-white"
             >
                 {title}
             </motion.p>
-            <motion.span
-                variants={textVariants}
-                className="text-lg text-gray-500 font-sans"
-            >
-                {genre}
-            </motion.span>
+            <div className="flex justify-between items-center">
+                <p className="font-sans text-xs text-gray-400">{genre}</p>
+                <div className="flex space-x-1 " style={{ fontSize: 10 }}>
+                    {new Array(5).fill('').map((item, index) => (
+                        <FontAwesomeIcon
+                            icon={faStar}
+                            key={`star-${index}`}
+                            className="text-yellow-400"
+                        />
+                    ))}
+                </div>
+            </div>
         </motion.div>
     )
 }
