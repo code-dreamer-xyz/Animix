7fc37d34b743eff948d43c7253beaf2f2b9c7963
Code Dreamer <codedreamer615@gmail.com>
2022-04-17 15:59:42 +0100

    add route + minor change

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -31,71 +31,78 @@ const Navbar: React.FC = () => {
             path: '/movies',
             page: 'Movies',
         },
+        {
+            path: '/blog',
+            page: 'Blog',
+        },
     ]
 
     return (
-        <header className="absolute z-50  top-0 w-full 2xl:px-0 px-2 py-1">
+        <header className="relative z-50  w-full 2xl:px-0 px-2 py-1">
             <nav className="max-w-screen-xl mx-auto flex justify-between items-center">
                 <Link href="/">
                     <a className="font-syne text-white text-xl">
                         Ani<span className="text-primary">Mix</span>
                     </a>
                 </Link>
-                <ul className=" justify-between md:flex hidden">
-                    {routes.map(({ path, page }) => (
-                        <motion.li
-                            whileTap={{ scale: 0.9 }}
-                            className="text-xl mx-4"
-                            key={path}
-                        >
-                            <Link href={path}>
-                                <a className="hover:text-primary cursor-pointer font-poppins font-bold text-white">
-                                    {page}
-                                </a>
-                            </Link>
-                        </motion.li>
-                    ))}
-                </ul>
-                <div className=" md:flex hidden">
-                    {!user && (
-                        <Button>
-                            <Link href="/login">
-                                <a className="text-white">Log In</a>
-                            </Link>
-                        </Button>
-                    )}
-                    {user && (
-                        <div
-                            className="relative cursor-pointer"
-                            onClick={() => setOpen(!open)}
-                        >
-                            <div className="flex space-x-6 items-center">
-                                <Avatar img={userProfile?.photoURL} />
-                                <p className="text-white font-poppins font-bold">
-                                    {userProfile?.userName}
-                                </p>
-                                <FontAwesomeIcon
-                                    icon={open ? faCaretUp : faCaretDown}
-                                    className="text-white"
-                                />
-                            </div>
-
-                            <div
-                                className={`absolute -bottom-30 bg-white w-full left-0  flex-col space-y-4 p-2 ${
-                                    open ? 'flex' : 'hidden'
-                                }`}
+                <div className="flex items-center space-x-4">
+                    <ul className=" justify-between md:flex hidden">
+                        {routes.map(({ path, page }) => (
+                            <motion.li
+                                whileTap={{ scale: 0.9 }}
+                                className="text-xl mx-4"
+                                key={path}
                             >
-                                <button>
-                                    <Link href="/dashboard">
-                                        <a>My Movies</a>
-                                    </Link>
-                                </button>
+                                <Link href={path}>
+                                    <a className="hover:text-primary cursor-pointer font-poppins font-bold text-white">
+                                        {page}
+                                    </a>
+                                </Link>
+                            </motion.li>
+                        ))}
+                    </ul>
+                    <div className="md:flex hidden">
+                        {!user && (
+                            <Button>
+                                <Link href="/login">
+                                    <a className="text-white">Log In</a>
+                                </Link>
+                            </Button>
+                        )}
+                        {user && (
+                            <div
+                                className="relative cursor-pointer"
+                                onClick={() => setOpen(!open)}
+                            >
+                                <div className="flex space-x-6 items-center">
+                                    <Avatar img={userProfile?.photoURL} />
+                                    <p className="text-white font-poppins font-bold">
+                                        {userProfile?.userName}
+                                    </p>
+                                    <FontAwesomeIcon
+                                        icon={open ? faCaretUp : faCaretDown}
+                                        className="text-white"
+                                    />
+                                </div>
 
-                                <button onClick={sign_Out}>Log Out</button>
+                                <div
+                                    className={`absolute -bottom-30 bg-white w-full left-0  flex-col space-y-4 p-2 ${
+                                        open ? 'flex' : 'hidden'
+                                    }`}
+                                >
+                                    <button>
+                                        <Link href="/dashboard">
+                                            <a>My Movies</a>
+                                        </Link>
+                                    </button>
+
+                                    <button onClick={sign_Out}>Log Out</button>
+                                </div>
                             </div>
-                        </div>
-                    )}
+                        )}
+                    </div>
                 </div>
+
                 <div className="md:hidden relative z-30">
                     <Hamburger
                         toggled={navIsOpen}
