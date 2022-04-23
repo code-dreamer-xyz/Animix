84df5685eadef08d892058160f75bf4a1b735b62
Code Dreamer <codedreamer615@gmail.com>
2022-04-23 14:16:54 +0100

    color change

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,31 +1,29 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-    purge: [
-        './pages/**/*.{js,ts,jsx,tsx}',
-        './components/**/*.{js,ts,jsx,tsx}',
-    ],
-    darkMode: false,
-    theme: {
-        extend: {
-            fontFamily: {
-                syne: ['Syne', ...defaultTheme.fontFamily.sans],
-                poppins: ['Poppins', ...defaultTheme.fontFamily.sans],
-                sans: ['Open Sans', ...defaultTheme.fontFamily.sans],
-            },
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false,
+  theme: {
+    extend: {
+      fontFamily: {
+        syne: ['Syne', ...defaultTheme.fontFamily.sans],
+        poppins: ['Poppins', ...defaultTheme.fontFamily.sans],
+        sans: ['Open Sans', ...defaultTheme.fontFamily.sans],
+      },
 
-            colors: {
-                primary: '#FF5733',
-                theme: '#1f1f1f',
-                overlay: 'rgba(0,0,0,.4)',
-            },
-        },
+      colors: {
+        primary: '#FF5733',
+        theme: '#1f1f1f',
+        primaryDark: '#292929',
+        overlay: 'rgba(0,0,0,.4)',
+      },
     },
-    variants: {
-        extend: {
-            opacity: ['disabled'],
-            cursor: ['disabled'],
-        },
+  },
+  variants: {
+    extend: {
+      opacity: ['disabled'],
+      cursor: ['disabled'],
     },
-    plugins: [],
+  },
+  plugins: [],
 }
