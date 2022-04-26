8f553a14b2c597a18328963356f835fae573a827
Code Dreamer <codedreamer615@gmail.com>
2022-04-26 17:21:34 +0100

    undefined check

--- components/blog/UserProfile.tsx
+++ components/blog/UserProfile.tsx
@@ -3,14 +3,14 @@ const UserProfile = ({ user }) => {
     <div className="flex items-center my-6 flex-col">
       <img
         className="block w-12 h-12 mb-4 rounded-full"
-        src={user.photoURL}
+        src={user?.photoURL}
         alt="user"
       />
 
       <h1 className="text-lg text-white mb-2 font-poppins">
-        {user.displayName}
+        {user?.displayName}
       </h1>
-      <p className="text-gray-300 mb-4">@{user.username}</p>
+      <p className="text-gray-300 mb-4">@{user?.username}</p>
     </div>
   )
 }
