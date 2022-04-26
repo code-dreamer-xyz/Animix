336009375c47801b6ca5c8f06fb3b015635e49d6
Code Dreamer <codedreamer615@gmail.com>
2022-04-26 17:21:45 +0100

    add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,8 +13,10 @@ module.exports = {
 
       colors: {
         primary: '#FF5733',
+        secondary: '#33539e',
         theme: '#1f1f1f',
         primaryDark: '#292929',
+        secondaryDark: '#1c1c1c',
         overlay: 'rgba(0,0,0,.4)',
       },
     },
