528ee24502398ec6cb63b66e61299be3c94920db
Code Dreamer <codedreamer615@gmail.com>
2022-04-27 15:56:18 +0100

    update: tailwindcss 3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false,
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
   theme: {
     extend: {
       fontFamily: {
