b19c1000518478c58d3f21ebb6be9bfc64fd3d87
Code Dreamer <codedreamer615@gmail.com>
2022-05-12 10:50:37 +0100

    change route

--- components/ImgSlider.tsx
+++ components/ImgSlider.tsx
@@ -18,7 +18,7 @@ const ImgSlider = ({ movies }) => {
   return (
     <Slider {...settings}>
       {movies?.map((movie) => (
-        <Link href={`/${movie.id}`} key={movie.id}>
+        <Link href={`/movie/${movie?.id}`} key={movie?.id}>
           <a className="relative inline-block">
             <Image src={movie.img} alt="anime" width={120} height={150} />
             <span className="absolute top-0 left-0  h-full w-full text-white text-xs font-poppins font-bold flex items-end text-center p-1 pb-3 justify-center bg-overlay">
